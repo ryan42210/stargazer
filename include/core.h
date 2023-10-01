26cc4ff9efa3ba6a7c8b00db2ca080c9709f6df6
Ryan Zhang <ryan@MacBookAir42210.local>
2023-09-30 23:45:07 -0500

    start adding custom matrix class

--- include/core.h
+++ include/core.h
@@ -1,18 +1,21 @@
 #ifndef CORE_H_
 #define CORE_H_
 
-#include <Eigen/Core>
-#include <Eigen/Dense>
+#include "matrix.h"
 
-using Vec2f = Eigen::Vector2f;
-using Vec2i = Eigen::Vector2i;
-using Vec3f = Eigen::Vector3f;
-using Vec3i = Eigen::Vector3i;
-using Vec4f = Eigen::Vector4f;
-using Vec4i = Eigen::Vector4i;
-using Vecf = Eigen::VectorXf;
-using Mat3f = Eigen::Matrix3f;
-using MatXf = Eigen::MatrixXf;
+// #include <Eigen/Core>
+// #include <Eigen/Dense>
+
+using Vec2f = Vector<float, 2>;
+using Vec2i = Vector<int, 2>;
+using Vec3f = Vector<float, 3>;
+using Vec3i = Vector<int, 3>;
+using Vec4f = Vector<float, 4>;
+using Vec4i = Vector<int, 4>;
+template<int n> using Vecf = Vector<int, n>;
+
+using Mat3f = Matrix<float, 3, 3>;
+template<int n> using MatXf = Matrix<float, n, n>;
 
 
 constexpr float PI = 3.14159265358979323846264338328;
